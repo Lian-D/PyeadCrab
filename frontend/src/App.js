a609b0014af50f9f4306dedcf311282e3fa1d575
glenn2 <glenn2@students.cs.ubc.ca>
2022-11-11 18:25:22 -0800

    add button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from "react";
 import DetailsPanel from "./components/DetailsPanel";
 import GraphPanel from "./components/GraphPanel";
