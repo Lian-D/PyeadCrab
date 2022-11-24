f301b1bd83a7442d05c33aa19916677c2919867d
liand <liand@student.ubc.ca>
2022-11-23 21:09:30 -0800

    added command based main.py in addition to finishing the output of static.py. also renamed mainDynamic.py to dynamic.py

--- backend/main.py
+++ backend/main.py
@@ -1,119 +1,14 @@
 import sys
-import os
-import ast
-import json
-
-# This is a function class map we use to confirm our data
-functionClassMap = []
-# This is a function definition table of our valid functions
-functionDefinitions = []
-
-variableMap = []
-
-# Parser function from https://stackoverflow.com/questions/72064609/how-can-i-retrieve-function-names-and-attributes-from-python-code-with-ast 
-# for getting function calls, 
-
-def parse(d, c):
-  def parse_chain(d, c, p=[]):
-     if isinstance(d, ast.Name):
-        return [d.id]+p
-     if isinstance(d, ast.Call):
-        for i in d.args:
-           parse(i, c)
-        return parse_chain(d.func, c, p)
-     if isinstance(d, ast.Attribute):
-        return parse_chain(d.value, c, [d.attr]+p)
-  if isinstance(d, (ast.Call, ast.Attribute)):
-     c.append('.'.join(parse_chain(d, c)))
-  else:
-     for i in getattr(d, '_fields', []):
-       if isinstance(t:=getattr(d, i), list):
-          for i in t:
-             parse(i, c)
-       else:
-          parse(t, c)
-
-def parseVariables(d, ret):
-    root = ast.parse(d)
-    ret += sorted({node.id for node in ast.walk(root) if isinstance(node, ast.Name) and not isinstance(node.ctx, ast.Load)})
-
-
-def functionGrab(className ,functionAst):
-    functionName = functionAst.name
-    # print(ast.dump(functionAst, indent=4))
-
-    # Add to function definitions map
-    functionDefinitions.append(functionName)
-    functionDefinitions.append(className+"."+functionName)
-
-    # Grab the parameters
-    functionArgs = []
-    for arg in functionAst.args.args:
-        functionArgs.append(arg.arg)
-
-    # if functionAst.args.vararg != None:
-    #     functionArgs.append("*" + functionAst.args.vararg.arg)
-
-    # if functionAst.args.kwarg != None:
-    #     functionArgs.append("**" + functionAst.args.kwarg.a)
-    # Grabs the functions that are called
-    callsArr = []
-    
-    parse(functionAst, callsArr)
-    parseVariables(functionAst, variableMap)
-    # Raw List of functions calls checked against our defined functions module
-    # callsArr = [value for value in callsArr if value in functionDefinitions]
-
-    # Creates our object
-    functionObj = {
-        "class": className,
-        "functionName": functionName,
-        "args": functionArgs,
-        "functionCalls": callsArr
-    }
-    # Appends to the map
-    functionClassMap.append(functionObj)
-
-def classGrab(classAst):
-    iterateClass(classAst)
-
-def iterateClass(classAst):
-    className = classAst.name
-    for x in classAst.body:
-        match x.__class__:
-            case ast.FunctionDef:
-                functionGrab(className, x)
-            case ast.ClassDef:
-                classGrab(x)
-
-def readRepo(repo):
-    # Open the module with the trace function and retrieve its AST
-    directoryArr = os.listdir(repo)
-    print(directoryArr)
-
-    for fileName in directoryArr:
-        file = repo+fileName
-        tracerFile = open(file,'r').read()
-        tracerTree = ast.parse(tracerFile)
-
-        for x in tracerTree.body:
-            match x.__class__:
-                case ast.FunctionDef:
-                    functionGrab(fileName,x)
-                case ast.ClassDef:
-                    classGrab(x)
-    print(functionClassMap)
-    print(functionDefinitions)
-    print(variableMap)
-
-def createForceGraphStructure(): 
-    pass
+import static
+# import dynamic
 
 def main():
     args = sys.argv[1:]
-    readRepo(sys.argv[1])
-    jsonOutput = json.dumps(functionClassMap, indent=4)
-    with open('../frontend/src/data/tempStatic.json', 'w+') as outfile:
-        outfile.write(jsonOutput)
+    if (sys.argv[2] == "-s"):
+        static.readRepo(sys.argv[1])
+    if (sys.argv[2] == "-d"):
+        pass
+    if (sys.argv[2] == "-sd"):
+        static.readRepo(sys.argv[1])
 
 main()
