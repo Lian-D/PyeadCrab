2fa68bb16806245ad31324ceff76b9e5d062b632
liand <liand@student.ubc.ca>
2022-11-23 21:13:48 -0800

    added command based main.py in addition to finishing the output of static.py. also renamed mainDynamic.py to dynamic.py

---
+++ backend/dynamic.py
@@ -0,0 +1,80 @@
+import sys
+import os
+import tracer
+import ast
+from pathlib import Path
+functionClassMap = []
+# This is a function definition table of our valid functions
+functionDefinitions = []
+def functionGrab(className ,functionAst):
+    functionName = functionAst.name
+    # Add to function definitions map
+    functionDefinitions.append(functionName)
+    functionDefinitions.append(className+"."+functionName)
+    # Grab the parameters
+    functionArgs = []
+    for arg in functionAst.args.args:
+        functionArgs.append(arg.arg)
+    if functionAst.args.vararg != None:
+        functionArgs.append("*" + functionAst.args.vararg.arg)
+    if functionAst.args.kwarg != None:
+        functionArgs.append("**" + functionAst.args.kwarg.arg)
+    # Creates our object
+    functionObj = {
+        "class": className,
+        "functionName": functionName,
+        "args": functionArgs
+    }
+    # Appends to the map
+    functionClassMap.append(functionObj)
+def classGrab(classAst):
+    iterateClass(classAst)
+def iterateClass(classAst):
+    className = classAst.name
+    for x in classAst.body:
+        match x.__class__:
+            case ast.FunctionDef:
+                functionGrab(className, x)
+            case ast.ClassDef:
+                classGrab(x)
+def readRepo(repo):
+    # Open the module with the trace function and retrieve its AST
+    directoryArr = os.listdir(repo)
+    
+    for fileName in directoryArr:
+        try:
+            file = repo+fileName
+            tracerFile = open(file,'r').read()
+            tracerTree = ast.parse(tracerFile)
+            for x in tracerTree.body:
+                match x.__class__:
+                    case ast.FunctionDef:
+                        functionGrab(fileName,x)
+                    case ast.ClassDef:
+                        classGrab(x)
+        except:
+            pass
+
+# Get path from command line arguments and insert it into the system
+dir_path = os.path.dirname(os.path.realpath(sys.argv[1]))
+sys.path.insert(0, dir_path)
+# Read the directory of the target program to produce map of user defined classes/modules and their functions
+readRepo(dir_path + "\\")
+# Pass map to tracer script and start the tracing
+tracer.setGlobal(functionClassMap)
+tracer.start()
+# Get path to target program from command line arguments
+targetPath = sys.argv[1]
+# Get name of entry point file
+targetEntryPoint = Path(targetPath).name
+# Get command line arguments for target program, if any
+targetCmdArgs = sys.argv[2:]
+finalTargetCmdArgs = [targetEntryPoint]
+for arg in targetCmdArgs:
+    finalTargetCmdArgs.append(arg)
+# Manually set command line arguments so that they are available for target program execution
+sys.argv = finalTargetCmdArgs
+exec(open(targetPath).read())
+tracer.fillInEntry(targetEntryPoint)
+print(tracer.callTrace)
+print(functionClassMap)
