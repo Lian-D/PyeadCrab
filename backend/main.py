89230609261c7a23ad3c3e2a28eeb506320cbef8
liand <liand@student.ubc.ca>
2022-11-27 14:06:11 -0800

    flipped command line

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,9 @@ import dynamic
 
 def main():
     args = sys.argv[1:]
-    if (sys.argv[2] == "-s"):
+    if (sys.argv[1] == "-s"):
         # static.readRepo(sys.argv[1])
-        static.execute(sys.argv[1])
-    elif (sys.argv[2] == "-d"):
-        dynamic.execute(sys.argv[1], sys.argv[3:])
+        static.execute(sys.argv[2])
+    elif (sys.argv[1] == "-d"):
+        dynamic.execute(sys.argv[2], sys.argv[3:])
 main()
